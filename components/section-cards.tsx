073becaa1039cb87b72ecd364d6e7128ade24cf4
Jeremy Sfez <jeremy@smooth-code.com>
2025-06-26 18:58:18 +0200

    feat: add new customers cards

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -1,4 +1,4 @@
-import { IconTrendingUp } from "@tabler/icons-react";
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react";
 
 import { Badge } from "@/components/ui/badge";
 import {
@@ -32,6 +32,28 @@ export function SectionCards() {
           </div>
           <div className="text-muted-foreground">
             Visitors for the last 6 months
+          </div>
+        </CardFooter>
+      </Card>
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>New Customers</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            1,234
+          </CardTitle>
+          <CardAction>
+            <Badge variant="outline">
+              <IconTrendingDown />
+              -20%
+            </Badge>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Down 20% this period <IconTrendingDown className="size-4" />
+          </div>
+          <div className="text-muted-foreground">
+            Acquisition needs attention
           </div>
         </CardFooter>
       </Card>
